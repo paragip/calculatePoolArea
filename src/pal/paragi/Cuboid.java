11a3e06d022240f6bf7221b8476dd778bc733446
paragip <pal.paragi@gmail.com>
2022-06-25 18:34:01 +0200

    calculation of pool area

---
+++ src/pal/paragi/Cuboid.java
@@ -0,0 +1,23 @@
+package pal.paragi;
+
+public class Cuboid extends Rectangle{
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public double getVolume() {
+        return getArea() * getHeight();
+    }
+
+}
